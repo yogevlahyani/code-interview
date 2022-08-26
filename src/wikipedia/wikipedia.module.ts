8d90c24d3f98ac2f73672c3875192e81aba062e7
Yogev Lahyani <yogevlahyani@Yogevs-MacBook-Pro.local>
2022-08-27 02:35:10 +0300

    Add default ttl to the cache module

--- src/wikipedia/wikipedia.module.ts
+++ src/wikipedia/wikipedia.module.ts
@@ -3,7 +3,7 @@ import { CacheModule, Module } from '@nestjs/common';
 import { WikipediaService } from './wikipedia.service';
 
 @Module({
-  imports: [CacheModule.register(), HttpModule],
+  imports: [CacheModule.register({ ttl: 3600 }), HttpModule],
   providers: [WikipediaService],
   exports: [WikipediaService],
 })
